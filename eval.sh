ff8fa3984d8d1134112b954af92931ca5c7f0547
kyujinHan <98331298+KyujinHan@users.noreply.github.com>
2025-07-05 20:41:49 +0900

    evaluation code

---
+++ eval.sh
@@ -0,0 +1,6 @@
+python eval.py \
+    --base_model [...VLM_model...] \
+    --huggingface_token [...huggingface_token...] \
+    --dataset_path ./data/Sampled_시각화_자료_질의응답_데이터_benchmark.csv \
+    --image_path /home/jovyan/share/19_VQA_datasets/images/ \
+    --cutoff_len 2048
