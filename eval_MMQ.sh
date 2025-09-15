e67fcfcdc3b50355f56d7fe334cbcd4cde861d8c
kyujinHan <98331298+KyujinHan@users.noreply.github.com>
2025-09-15 15:26:53 +0900

    Update eval_MMQ.sh

--- eval_MMQ.sh
+++ eval_MMQ.sh
@@ -2,7 +2,7 @@
 
 python eval_MMQ.py \
     --base_model AIDC-AI/Ovis2.5-2B \
-    --thinking_mode \
+    --thinking_mode False \
     --huggingface_token [...your_token...] \
     --dataset_path ./data/Gemini_sampled_멀티모달_정보검색_데이터_benchmark_final.csv \
     --image_path ./data/images/ \
