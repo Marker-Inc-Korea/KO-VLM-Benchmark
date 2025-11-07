5f66230673c285b84ba25a42d3230c291e3631af
kyujinHan <98331298+KyujinHan@users.noreply.github.com>
2025-11-07 13:40:37 +0900

    Delete eval_MMQ.sh

--- eval_MMQ.sh
+++
@@ -1,9 +0,0 @@
-# Markr-AI/Gukbap-Ovis2-16B-VL
-
-python eval_MMQ.py \
-    --base_model AIDC-AI/Ovis2.5-2B \
-    --thinking_mode False \
-    --huggingface_token [...your_token...] \
-    --dataset_path ./data/Gemini_sampled_멀티모달_정보검색_데이터_benchmark_final.csv \
-    --image_path ./data/images/ \
-    --cutoff_len 2048
