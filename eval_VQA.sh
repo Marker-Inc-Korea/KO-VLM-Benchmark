e56820f99fd2203daacb33cd5eb2fa665a4321ec
kyujinHan <98331298+KyujinHan@users.noreply.github.com>
2025-11-07 13:40:46 +0900

    Delete eval_VQA.sh

--- eval_VQA.sh
+++
@@ -1,6 +0,0 @@
-python eval_VQA.py \
-    --base_model [...VLM_model...] \
-    --huggingface_token [...huggingface_token...] \
-    --dataset_path ./data/Sampled_시각화_자료_질의응답_데이터_benchmark.csv \
-    --image_path ./data/images \
-    --cutoff_len 2048
